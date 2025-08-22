4524c200cd59eb48cd7fafcceaaa818c17750fd6
TatiRR3 <tatiana.rodriguez010397@gmail.com>
2025-08-22 20:35:30 -0300

    Estructura desde URL productos

--- js/products.js
+++ js/products.js
@@ -1,23 +1,29 @@
 const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"
-let getJSONData = function(url){
-    let result = {};
 
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        return result;
-    });
+    document.addEventListener("DOMContentLoaded", function () {
+  getJSONData(URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      let products = resultObj.data.products;
+      mostrarProductos(products);
+    }
+  });
+});
+
+function mostrarProductos(products) {
+  const contenedor = document.getElementById("lista-productos");
+  contenedor.innerHTML = "";
+
+  for (let product of products) {
+    contenedor.innerHTML += `
+      <div class="product-card">
+        <img src="${product.image}" alt="${product.name}">
+        <div>
+          <h4>${product.name}</h4>
+          <p>${product.description}</p>
+          <p><strong>Precio:</strong> ${product.currency} ${product.cost}</p>
+          <p><strong>Vendidos:</strong> ${product.soldCount}</p>
+        </div>
+      </div>
+    `;
+  }
 }
