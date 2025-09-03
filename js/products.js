ea6f7e8e67641a8c8d862a71bdec054244adb0f6
EugeZeintu <maugezeintunsian@gmail.com>
2025-09-03 11:25:50 -0300

    Cards imagen centrada

--- js/products.js
+++ js/products.js
@@ -37,21 +37,15 @@ function mostrarProductos(products) {
 
   for (let product of products) {
     contenedor.innerHTML += `
-                     <div class="col-sm-6 col-md-6 col-lg-4 mb-4">
-                     <div class="card mb-3" style="max-width: 540px; cursor: pointer;" onclick="setCatID(${product.id})">
-          <div class="row g-0">
-          <div class="col-md-4">
-      <img src="${product.image}" class="img-fluid rounded-start" alt="${product.description}">
-    </div>
-    <div class="col-md-8">
+    <div class="col-sm-6 col-md-6 col-lg-4 mb-4">
+    <div class="card mb-4" style="max-width: 540px; cursor: pointer;" onclick="setCatID(${product.id})">
       <div class="card-body">
+      <img src="${product.image}" class="img-fluid rounded-start align-items-center" alt="${product.description}">
        <h5 class="card-title">${product.name}</h5>
        <p class="card-text">${product.description}</p>
        <p class="card-text"><strong>${product.currency} ${product.cost}</strong></p>
        <p class="card-text"><small class="text-muted">${product.soldCount} vendidos</small></p>
       </div>
-    </div>
-  </div>
 </div>
 `;
   }
